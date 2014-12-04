f06d59e66da181882747bbf026592090ec6b831d
Brock Riemenschneider <brock@mvpdesign.com>
2014-12-04 10:23:33 -0600

    Formatted to PSR guidelines

--- library/Thms/Config/Environment.php
+++ library/Thms/Config/Environment.php
@@ -5,125 +5,117 @@ use Closure;
 
 class Environment
 {
-	/**
-	 * Root path where files are located.
-	 * 
-	 * @var string
-	 */
-	protected $path;
+    /**
+     * Root path where files are located.
+     *
+     * @var string
+     */
+    protected $path;
 
-	/**
-	 * Environments locations.
-	 * 
-	 * @var array|Closure
-	 */
-	protected $locations = array();
+    /**
+     * Environments locations.
+     *
+     * @var array|Closure
+     */
+    protected $locations = array();
 
-	/**
-	 * Init the Environment class.
-	 * 
-	 * @param string $path The root path where environments files are located.
-	 * @param array|Closure $locations Environment locations - Detection.
-	 */ 
-	public function __construct($path, $locations)
-	{
-		$this->path = $path;
-		$this->locations = $locations;
-	}
+    /**
+     * Init the Environment class.
+     *
+     * @param string $path The root path where environments files are located.
+     * @param array|Closure $locations Environment locations - Detection.
+     */
+    public function __construct($path, $locations)
+    {
+        $this->path = $path;
+        $this->locations = $locations;
+    }
 
-	/**
-	 * Find which environment we are.
-	 * 
-	 * @return string
-	 */
-	public function which()
-	{
+    /**
+     * Find which environment we are.
+     *
+     * @return string
+     */
+    public function which()
+    {
         // Check if $locations is a closure.
         // This means we're checking for environment variables through the closure.
-        if ($this->locations instanceof Closure)
-        {
+        if ($this->locations instanceof Closure) {
             $callback = $this->locations;
             return $callback();
         }
 
         // If not using closure, we're using the default detection
         // by comparing the hostname.
-		$hostname = gethostname();
+        $hostname = gethostname();
 
-		foreach ($this->locations as $location => $name)
-		{
-			if ($hostname === $name)
-			{
-				return $location;
-			}
-		}
+        foreach ($this->locations as $location => $name) {
+            if ($hostname === $name) {
+                return $location;
+            }
+        }
 
         return '';
-	}
+    }
 
-	/**
-	 * Load the .env.{$location}.php file.
-	 * 
-	 * @param string $location
-	 * @return array
-	 */
-	public function load($location)
-	{
-		if (file_exists($path = $this->getFile($location)))
-		{
+    /**
+     * Load the .env.{$location}.php file.
+     *
+     * @param string $location
+     * @return array
+     */
+    public function load($location)
+    {
+        if (file_exists($path = $this->getFile($location))) {
             return require_once($path);
-		}
+        }
 
-		return array();
-	}
+        return array();
+    }
 
-	/**
-	 * Check required values.
-	 * 
-	 * @param array $required The required values to check.
-	 * @param array $values
-	 * @return bool
-	 */
-	public function check(array $required, array $values)
-	{
-		foreach ($required as $key)
-		{
-			if (!array_key_exists($key, $values))
-			{
-				return false;
-			}
-		}
+    /**
+     * Check required values.
+     *
+     * @param array $required The required values to check.
+     * @param array $values
+     * @return bool
+     */
+    public function check(array $required, array $values)
+    {
+        foreach ($required as $key) {
+            if (! array_key_exists($key, $values)) {
+                return false;
+            }
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * Populate environment vars.
-	 * 
-	 * @param array $vars The loaded environments vars.
-	 * @return void
-	 */
-	public function populate(array $vars)
-	{
-		foreach ($vars as $key => $value)
-		{
-			if (false === getenv($key))
-			{
-				$_ENV[$key] = $value;
-				$_SERVER[$key] = $value;
-				putenv("{$key}={$value}");
-			}
-		}
-	}
+    /**
+     * Populate environment vars.
+     *
+     * @param array $vars The loaded environments vars.
+     * @return void
+     */
+    public function populate(array $vars)
+    {
+        foreach ($vars as $key => $value) {
+            if (false === getenv($key)) {
+                $_ENV[$key] = $value;
+                $_SERVER[$key] = $value;
+                putenv("{$key}={$value}");
+            }
+        }
+    }
 
-	/**
-	 * Return the .env file path.
-	 * 
-	 * @param string $location
-	 * @return string
-	 */
-	protected function getFile($location)
-	{
-		return $this->path.'.env.'.$location.'.php';
-	}
+    /**
+     * Return the .env file path.
+     *
+     * @param string $location
+     * @return string
+     */
+    protected function getFile($location)
+    {
+        return $this->path.'.env.'.$location.'.php';
+    }
 }
